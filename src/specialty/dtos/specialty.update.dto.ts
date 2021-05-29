3a4eb7d6ae79880451fbc7a57ae1a9e7e8e35b39
Victor Campelo <victor_campelo@outlook.com>
2021-05-29 18:29:45 -0300

    feat: finalize crud to specialty entity

--- src/specialty/dtos/specialty.update.dto.ts
+++ src/specialty/dtos/specialty.update.dto.ts
@@ -1,6 +1,9 @@
-import { MaxLength } from 'class-validator';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class UpdateSpecialtyDto {
+  @IsNotEmpty({
+    message: 'Por favor, adicione o novo nome da especialidade.',
+  })
   @MaxLength(255, {
     message: 'O nome da especialidade deve conter até 255 caracteres',
   })
--- src/specialty/specialty.controller.ts
+++ src/specialty/specialty.controller.ts
@@ -10,7 +10,12 @@ import {
   HttpCode,
   Body,
   ValidationPipe,
+  Delete,
+  Get,
+  Param,
+  Put,
 } from '@nestjs/common';
+import { UpdateSpecialtyDto } from './dtos/specialty.update.dto';
 
 @ApiTags('Specialty')
 @Controller('specialtys')
@@ -20,15 +25,51 @@ export class SpecialtyController {
   /**
    * Posts specialtys controller
    * @param createSpecialtyDto
-   * @returns specialty
+   * @returns created specialty
    */
   @Post('/create')
-  @UseInterceptors(ClassSerializerInterceptor)
   @HttpCode(201)
   async createSpecialty(
     @Body(new ValidationPipe({ transform: true }))
     createSpecialtyDto: CreateSpecialtyDto,
   ): Promise<Specialty> {
     return await this.specialtyService.create(createSpecialtyDto);
+  }
+
+  /**
+   * Gets all specialties controller
+   * @returns list of all specialties
+   */
+  @Get('/')
+  @UseInterceptors(ClassSerializerInterceptor)
+  @HttpCode(200)
+  async showSpecialties(): Promise<any[]> {
+    return await this.specialtyService.findAll();
+  }
+
+  /**
+   * Puts specialties controller
+   * @param id
+   * @param updateSpecialtyDto
+   * @returns updated specialty
+   */
+  @Put('/edit/:id')
+  @HttpCode(200)
+  async editSpecialty(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateSpecialtyDto: UpdateSpecialtyDto,
+  ): Promise<Specialty> {
+    return await this.specialtyService.update(id, updateSpecialtyDto);
+  }
+
+  /**
+   * Deletes specialties controller
+   * @param id
+   * @returns message
+   */
+  @Delete('/delete/:id')
+  @HttpCode(200)
+  async deleteSpecialty(@Param('id') id: string): Promise<string> {
+    return await this.specialtyService.delete(id);
   }
 }
