753cc461ea14c7ff729fa64178d3f0fde9f9e69c
Victor Campelo <victor_campelo@outlook.com>
2021-05-30 13:17:35 -0300

    feat: repository to find doctor by multiples attributes

---
+++ src/doctor/dtos/doctor.find.dto.ts
@@ -0,0 +1,38 @@
+import { IsNumberString } from 'class-validator';
+
+export class FindDoctorDto {
+  id?: string;
+
+  nome?: string;
+
+  crm?: string;
+
+  @IsNumberString({ no_symbols: true })
+  telefone_fixo?: string;
+
+  @IsNumberString({ no_symbols: true })
+  telefone_celular?: string;
+
+  @IsNumberString({ no_symbols: true })
+  cep?: string;
+
+  logradouro?: string;
+
+  complemento?: string;
+
+  bairro?: string;
+
+  cidade?: string;
+
+  estado?: string;
+
+  ibge?: string;
+
+  gia?: string;
+
+  ddd?: string;
+
+  siafi?: string;
+
+  especialidades?: string[];
+}
